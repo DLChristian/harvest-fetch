b7676463ff09509da55cdfd5a5addec35fe6df66
Lenore <lenorekelly2@gmail.com>
2023-03-28 13:50:18 -0500

    adding nav

--- harvest-fetch-client/src/components/Navbar.js
+++ harvest-fetch-client/src/components/Navbar.js
@@ -12,17 +12,17 @@ function Navbar(){
                         <Link to="/" className="nav-link">Home</Link>
                     </li>
                     <li className="nav-item">
-                        <a class="nav-link" href="#">Features</a>
+                        <a class="nav-link" href="#">Select Your Address</a>
                     </li>
                     <li className="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
+                        <a class="nav-link" href="#">Login</a>
                     </li>
                 </ul>
                 <form className="form-inline">
                     <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
                     <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                 </form>
-                </div>
+            </div>
         </nav>
         );
 }
