f99b5bd5bfeea3768825f0150fee39746a1a1576
Lenore <lenorekelly2@gmail.com>
2023-03-29 09:45:09 -0500

    adding everything

--- harvest-fetch-client/src/components/Navbar.js
+++ harvest-fetch-client/src/components/Navbar.js
@@ -14,7 +14,7 @@ function Navbar(){
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item"><Link class="nav-link active" aria-current="page" to="/">Home</Link></li>
-                        <li class="nav-item"><Link class="nav-link" to="/">Select your address</Link></li>
+                        <li class="nav-item"><Link class="nav-link" to="/">Farmers</Link></li>
                         <li class="nav-item"><Link class="nav-link" to="/">Login</Link></li>
                     </ul>
                     <form class="d-flex bar" role="search">
