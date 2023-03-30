efd40579d4066501352266c6506524f9bb797931
Lenore <lenorekelly2@gmail.com>
2023-03-30 13:53:50 -0500

    adding everything

--- Harvest-Fetch-API/src/main/java/Harvest/Controllers/ProductController.java
+++ Harvest-Fetch-API/src/main/java/Harvest/Controllers/ProductController.java
@@ -1,2 +1,26 @@
-package Harvest.Controllers;public class ProductController {
+package Harvest.Controllers;
+
+import Harvest.Domain.ProductService;
+import Harvest.Models.Product;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/product")
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService){
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> findAll(){
+        return productService.findAll();
+    }
 }
