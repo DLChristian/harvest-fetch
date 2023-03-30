6cd3ed758080b1c18331a6672dfcee4c7a457d53
Lenore <lenorekelly2@gmail.com>
2023-03-30 14:19:18 -0500

    adding everything

--- Harvest-Fetch-API/src/main/java/Harvest/Controllers/ProductController.java
+++ Harvest-Fetch-API/src/main/java/Harvest/Controllers/ProductController.java
@@ -1,11 +1,11 @@
 package Harvest.Controllers;
 
 import Harvest.Domain.ProductService;
+import Harvest.Domain.Result;
 import Harvest.Models.Product;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +22,44 @@ public class ProductController {
     @GetMapping
     public List<Product> findAll(){
         return productService.findAll();
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<Product> findById(@PathVariable int productId) {
+        Product product = productService.findById(productId);
+        if (product == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(product);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> add(@RequestBody Product product) {
+        Result<Product> result = productService.add(product);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
+        }
+        return ErrorResponse.build(result);
+    }
+
+    @PutMapping("/{productId}")
+    public ResponseEntity<Object> update(@PathVariable int productId, @RequestBody Product product) {
+        if (productId != product.getProductId()) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
+        Result<Product> result = productService.update(product);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return ErrorResponse.build(result);
+    }
+
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<Void> deleteById(@PathVariable int productId) {
+        if (productService.deleteById(productId)) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
