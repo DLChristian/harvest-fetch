37eec18d9f54ab3c8348975b08a36eb5f1ac3387
Stephanie Drake <drake.steph37@gmail.com>
2023-03-31 18:11:01 -0400

    login and login css, also start of signin form

--- harvest-fetch-client/src/App.js
+++ harvest-fetch-client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState, useEffect} from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -6,18 +7,53 @@ import {
 } from "react-router-dom";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import { refresh } from "./services/authService";
+import AuthContext from "./contexts/AuthContext";
+import LoginForm from "./pages/LoginForm";
+import SignupForm from "./pages/SignupForm"
 
 function App() {
+
+  const [appUser, setAppUser] = useState();
+
+  useEffect(() => {
+    refresh().then(login).catch();
+  }, [])
+
+  function login(appUserArg) {
+    setAppUser(appUserArg);
+    localStorage.setItem("HF_JWT", appUserArg.jwt);
+  }
+
+  function logout() {
+    setAppUser();
+    localStorage.removeItem("HF_JWT");
+  }
+
+  const auth = {
+    appUser,
+    login,
+    logout
+
+  };
+
+
+
   return (
+    
     <div className="App">
+      <AuthContext.Provider value={auth}>
       <Router>
         <Navbar />
         <div>
           <Routes>
             <Route path="/" element={<Home />}/>
+            <Route path="/loginform" element={<LoginForm />} />
+            <Route path="/signupform" element={<SignupForm />} />
           </Routes>
         </div>
       </Router>
+      </AuthContext.Provider>
     </div>
   );
 }
--- harvest-fetch-client/src/pages/SignupForm.js
+++ harvest-fetch-client/src/pages/SignupForm.js
@@ -1,15 +1,99 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
-function SignupForm() {
+export default function SignupForm() {
 
-
-
-
-   
-
-    const [user_name, setUser_name] = useState('');
+    const [firstname, setFirstName] = useState('');
+    const [lastname, setLastName] = useState('');
+    const [address, setAddress] = useState('');
+    const [zipCode, setZipCode] = useState('');
+    const [city, setCity] = useState('');
+    const [state, setState] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [photoUrl, setPhotoUrl] = ('');
+    const [userName, setUserName] = useState('');
     const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+    
+
+    const handleSubmit =  (event) => {
+        event.preventDefault();
+            
+    };
+    return (
+
+        <form onSubmit={handleSubmit}>
+            <div className="form-container">
+                    <h2>Create Account</h2>
+                    <div className="mb-3">
+                        <label className= "form-label" htmlFor="firstname">First Name</label>
+                        <input type="text" className="form-control" id="firstname" name="firstname"
+                            value={firstname}  />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label" htmlFor="lastname">Last Name</label>
+                        <input type="text" className="form-control" id="lastname" name="lastname"
+                            value={lastname}  />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label" htmlFor="address">Address</label>
+                        <input type="text" className="form-control" id="address" name="address"
+                            value={address}  />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label" htmlFor="zipCode">Zip Code</label>
+                        <input type="text" className="form-control" id="zipCode" name="zipCode"
+                            value={zipCode}  />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label" htmlFor="city">City</label>
+                        <input type="text" className="form-control" id="city" name="city"
+                            value={city}  />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label" htmlFor="state">State</label>
+                        <input type="text" className="form-control" id="state" name="state"
+                            value={state}  />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label" htmlFor="email">Email</label>
+                        <input type="email" className="form-control" id="email" name="email"
+                            value={email}  />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label" htmlFor="phone">Phone Number</label>
+                        <input type="text" className="form-control" id="phone" name="phone"
+                            value={phone}  />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label" htmlFor="photoUrl">Photo</label>
+                        <input type="text" className="form-control" id="photoUrl" name="photoUrl"
+                            value={photoUrl}  />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label" htmlFor="userName">UserName</label>
+                        <input type="text" className="form-control" id="userName" name="userName"
+                            value={userName}  />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label" htmlFor="password">Password</label>
+                        <input type="password" className="form-control" id="password" name="password"
+                            value={password}  />
+                    </div>
+                    <div className="mb-3">
+                        <Link to="/" className="btn btn-warning me-1">Cancel</Link>
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </div>
+                                        
+            </div>        
+        </form>
+    );
+
+    
+   
     
 
     
