68c148de3f28ec7b1f89c9d6699885e09e77a551
Stephanie Drake <drake.steph37@gmail.com>
2023-04-02 17:54:19 -0400

    added Stripe client Side

---
+++ harvest-fetch-client/src/stripe/Success.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Success() {
+  return (
+    <div>
+      <h1>Payment Successful</h1>
+      <p>Thank you for your purchase!</p>
+      <p><a href="/">Return to Homepage</a></p>
+    </div>
+  );
+}
+
+export default Success;
