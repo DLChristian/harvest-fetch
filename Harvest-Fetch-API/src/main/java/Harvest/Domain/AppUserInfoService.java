f5caea4027603b0224017238abb31a9a79e4a966
Lenore <lenorekelly2@gmail.com>
2023-04-03 13:14:14 -0500

    adding app user domain

--- Harvest-Fetch-API/src/main/java/Harvest/Domain/AppUserInfoService.java
+++ Harvest-Fetch-API/src/main/java/Harvest/Domain/AppUserInfoService.java
@@ -22,21 +22,88 @@ public class AppUserInfoService {
         return repository.findAll();
     }
 
-//    public Result<AppUserInfo> add(AppUserInfo appUserInfo) {
-//        Result<AppUserInfo> result = validate(farmer);
-//        if (!result.isSuccess()) {
-//            return result;
-//        }
-//
-//        if (farmer.getFarmerId() != 0) {
-//            result.addMessage("farmerId cannot be set for `add` operation", ResultType.INVALID);
-//            return result;
-//        }
-//
-//        farmer = repository.add(farmer);
-//        result.setPayload(farmer);
-//        return result;
-//    }
+    public Result<AppUserInfo> add(AppUserInfo appUserInfo) {
+        Result<AppUserInfo> result = validate(appUserInfo);
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        if (appUserInfo.getAppUserId() != 0) {
+            result.addMessage("appUserId cannot be set for `add` operation", ResultType.INVALID);
+            return result;
+        }
+
+        appUserInfo = repository.add(appUserInfo);
+        result.setPayload(appUserInfo);
+        return result;
+    }
+
+    public Result<AppUserInfo> update(AppUserInfo appUserInfo) {
+        Result<AppUserInfo> result = validate(appUserInfo);
+        if (!result.isSuccess()) {
+            return result;
+        }
+        if (appUserInfo.getAppUserId() <= 0) {
+            result.addMessage("appUserId must be set for `update` operation", ResultType.INVALID);
+            return result;
+        }
+        if (!repository.update(appUserInfo)) {
+            String msg = String.format("appUserId: %s, not found", appUserInfo.getAppUserId());
+            result.addMessage(msg, ResultType.NOT_FOUND);
+        }
+
+        return result;
+    }
+
+    public Result deleteById(int appUserId) {
+        Result result = new Result();
+        if (!repository.deleteById(appUserId)) {
+            result.addMessage("App User id " + appUserId + "was not found.", ResultType.NOT_FOUND);
+        }
+        return result;
+    }
+
+    private Result<AppUserInfo> validate(AppUserInfo appUserInfo) {
+        Result<AppUserInfo> result = new Result<>();
+        if (appUserInfo == null) {
+            result.addMessage("appUserInfo cannot be null", ResultType.INVALID);
+            return result;
+        }
+
+        if (Validations.isNullOrBlank(appUserInfo.getFirstName())) {
+            result.addMessage("First name is required", ResultType.INVALID);
+        }
+
+        if (Validations.isNullOrBlank(appUserInfo.getLastName())) {
+            result.addMessage("Last Name is required", ResultType.INVALID);
+        }
+
+        if (Validations.isNullOrBlank(appUserInfo.getAddress())) {
+            result.addMessage("Street Address is required", ResultType.INVALID);
+        }
+
+        if (Validations.isNullOrBlank(appUserInfo.getZipCode())) {
+            result.addMessage("Zipcode is required", ResultType.INVALID);
+        }
+
+        if (Validations.isNullOrBlank(appUserInfo.getCity())) {
+            result.addMessage("City is required", ResultType.INVALID);
+        }
+
+        if (Validations.isNullOrBlank(appUserInfo.getState())) {
+            result.addMessage("State is required", ResultType.INVALID);
+        }
+
+        if (Validations.isNullOrBlank(appUserInfo.getEmail())) {
+            result.addMessage("Email is required", ResultType.INVALID);
+        }
+
+        if (Validations.isNullOrBlank(appUserInfo.getPhone())) {
+            result.addMessage("Phone number is required", ResultType.INVALID);
+        }
+
+        return result;
+    }
 
 
 }
